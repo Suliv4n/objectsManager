1b8c81a685505fc5b81f7c22aa98f5564ff9b4d8
Morgan Bourbon <morgan_bourbon@hotmail.fr>
2014-02-05 14:33:42 +0100

    Ajout des fonctionnalites de la classe personne

---
+++ app/models/Personne.java
@@ -0,0 +1,17 @@
+public class Personne{
+	private String nom;
+	private String inventaire;
+	
+	public Personne(String nom, String inventaire){
+			this.nom = nom;
+			this.inventaire = inventaire;
+	}
+	
+	public String getNom(){
+			return nom;
+	}
+	
+	public String getInventaire(){
+			return inventaire;
+	}			
+}	
